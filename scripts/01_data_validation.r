3091598c151400b40571ebc9dd1c3aec6a8e7ca3
Murray Hyde <jmhyde891@gmail.com>
2026-04-13 12:44:40 -0400

    refactor: remove loading logic to helper file

--- scripts/01_data_validation.r
+++ scripts/01_data_validation.r
@@ -23,7 +23,9 @@ global_paths = import("config.paths", convert = TRUE)
 config_file = as.character(global_paths$CONFIG / 'config.yaml')
 config = yaml.load_file(config_file)
 
-sampling_dates = config$sites$snakeden$sampling_occasions
+# Custom library
+custom_path = path(global_paths$SRC, '01_data_validation.r')
+source(custom_path)
 
 # -----------------------------------------------------------------------------
 # Paths and import/export directories
@@ -50,45 +52,10 @@ dir_create(c(
 # Load data
 # -----------------------------------------------------------------------------
 
-# --- Create lookup table for loading data ---
-# Intermediate table of project level sampling info
-occasion_info <- imap_dfr(config$sites, function(site_data, site_key) {
-  tibble(
-    site_key  = site_key,                          # "snakeden", "glade"
-    site_name = site_data$name,                    # "Snakeden Run", "Glade Run"
-    occasion  = map_int(site_data$sampling_occasions, "occasion"),
-    date      = map_chr(site_data$sampling_occasions, "date") |> as.Date()
-  )
-})
-# Lookup sheets/ranges for pulling data
-sheet_ranges = imap_dfr(config$sheet_ranges, function(occasions, site_key){
-  bind_rows(occasions) |> 
-    mutate(site_key = site_key)
-})
-# Join occasion info and sheet ranges for final lookup table
-occasions = sheet_ranges |> 
-  left_join(occasion_info, by = c('site_key', 'occasion'))
+# --- Create lookup table for adding site/occasion columns to loaded data ---
+occasions_lookup = build_occasions_lookup(config)
 
 # --- Read encounter data in long format ---
 input_file = path(source_folder, '2024 and 2025 MR Summary - 1.xlsx')
-data = occasions |> 
-  pmap(function(occasion, sheet, range, expected_rows, site_key, site_name, date, ...){
-    df = read_excel(input_file, sheet = sheet, range = range, col_types = 'text') |> 
-      mutate(
-        site = site_key,
-        occasion = occasion,
-        date = date
-      )
-    
-    # Confirm that the correct number of rows is imported 
-    message(glue(" {site_key} | {occasion} | {nrow(df)} rows imported"))
-    
-    
-    if (nrow(df) != expected_rows) {
-      warning(glue("  Row count mismatch for {site_name} occasion {occasion}: got {nrow(df)}, expected {expected_rows}"))
-    }
-
-    return(df)
-  }) |> 
-  list_rbind()
+encounter_data = read_encounter_data(occasions_lookup, input_file)
 
---
+++ src/01_data_validation.r
@@ -0,0 +1,47 @@
+# Functions used in 01_data_validation.r
+
+build_occasions_lookup = function(config){
+
+  # Intermediate table of project level sampling info
+  occasion_info <- imap_dfr(config$sites, function(site_data, site_key) {
+    tibble(
+      site_key  = site_key,                          # "snakeden", "glade"
+      site_name = site_data$name,                    # "Snakeden Run", "Glade Run"
+      occasion  = map_int(site_data$sampling_occasions, "occasion"),
+      date      = map_chr(site_data$sampling_occasions, "date") |> as.Date()
+    )
+  })
+  # Lookup sheets/ranges for pulling data
+  sheet_ranges = imap_dfr(config$sheet_ranges, function(occasions, site_key){
+    bind_rows(occasions) |> 
+      mutate(site_key = site_key)
+  })
+  # Join occasion info and sheet ranges for final lookup table
+  occasions = sheet_ranges |> 
+    left_join(occasion_info, by = c('site_key', 'occasion'))
+}
+
+read_encounter_data = function(occasions_lookup, input_file){
+occasions_lookup |> 
+  pmap(function(occasion, sheet, range, expected_rows, site_key, site_name, date, ...){
+    df = read_excel(input_file, sheet = sheet, range = range, col_types = 'text') |> 
+      mutate(
+        site = site_key,
+        occasion = occasion,
+        date = date
+      )
+    
+    # Confirm that the correct number of rows is imported 
+    message(glue(" {site_key} | {occasion} | {nrow(df)} rows imported"))
+    
+    
+    if (nrow(df) != expected_rows) {
+      warning(glue("  Row count mismatch for {site_name} occasion {occasion}: got {nrow(df)}, expected {expected_rows}"))
+    }
+
+    return(df)
+  }) |> 
+  list_rbind()
+
+
+}
