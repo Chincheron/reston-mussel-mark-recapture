45706278b3fd7c7b5c96dc42b6ab91611e6d1359
Murray Hyde <jmhyde891@gmail.com>
2026-06-02 10:03:21 -0400

    feat: initial graphing

--- scripts/05_top_model_results.r
+++ scripts/05_top_model_results.r
@@ -28,7 +28,9 @@ config = load_config()
 
 # Custom library
 custom_lib_1 = path(global_paths$SRC, '05_top_model_results.r')
+custom_lib_2 = path(global_paths$CONFIG, 'global_figure_config.r')
 source(custom_lib_1)
+source(custom_lib_2)
 
 # -----------------------------------------------------------------------------
 # Paths and import/export directories
@@ -39,11 +41,13 @@ SCRIPT_NAME = '05_top_model_results'
 source_folder = path(global_paths$DATA_PIPELINE)
 pipeline_folder = path(global_paths$DATA_PIPELINE, SCRIPT_NAME)
 interim_folder = path(global_paths$DATA_INTERIM, SCRIPT_NAME)
+figure_folder = path(global_paths$RESULTS_FIGURES, SCRIPT_NAME)
 
 # Make directories
 dir_create(c(
   pipeline_folder,
-  interim_folder
+  interim_folder,
+  figure_folder
   )
 )
 
@@ -88,6 +92,8 @@ top_model_results <- purrr::map_dfr(
 source(custom_lib_1)
 top_model_results = process_model_results(top_model_results)
 
+#TODO calculate derived population size
+
 # =============================================================================
 # 3. Export top model results 
 # =============================================================================
@@ -103,3 +109,51 @@ write_xlsx(top_model_results, data_save_path)
 # Export R object for later use
 data_objects_path = path(interim_folder, '05_top_model_results.rds')
 saveRDS(results_list, data_objects_path, ascii = TRUE)
+
+# =============================================================================
+# 4. Plot top model results
+# =============================================================================
+
+# All figures are exported to the specified figure_folder
+
+# -----------------------------------------------------------------------------
+# Get global plot variables and settings
+# -----------------------------------------------------------------------------
+
+source(custom_lib_2)
+# Specifies settings for visual aspects of all figures
+all_plot_config <- get_global_fig_config()
+# Pull column mapping for ease of reading function later
+cm = all_plot_config$column_mapping
+
+# Define initial settings for group of figures
+survival_plot_config <- list(
+  parameter = "S",
+  y_factor = cm$parameter_estimate,
+  y_label   = "Estimated Survival",
+  y_variance_upper = cm$s_upper_ci,
+  y_variance_lower = cm$s_lower_ci,
+  x_factor = cm$sampling_occasion,
+  x_factor_label = all_plot_config$labels$Occasion,
+  x_order = all_plot_config$category_order$sampling_occasion_s,
+  grouping = cm$site,
+  grouping_label = all_plot_config$labels$site,
+  grouping_order = c('glade', 'snakeden'),
+  grouping_palette = "site_level",
+  #NULL if 0 facets, 1 if single. If 2, then first will be rows and second columns
+  #facet_vars = c(cm$species),
+  title = NULL,
+  subtitle = NULL,
+  caption = NULL,
+  save_folder = figure_folder,
+  save_file_name = NULL,
+  aggregate_flag = FALSE,
+  variance_flag = TRUE
+)
+
+source(custom_lib_1)
+# --- Survival ---
+config_override = list(
+  save_file_name = 'Figure_1_abundance_top_model.jpg' 
+)
+build_base_plot(top_model_results, all_plot_config, survival_plot_config, config_override)
--- src/05_top_model_results.r
+++ src/05_top_model_results.r
@@ -1,3 +1,14 @@
+library(reticulate)
+library(withr)
+library(fs)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(writexl)
+library(scales)
+library(ggh4x)
+
+
 extract_rmark_model_results = function(data, analysis, model_name) {
   
   real_results = results_list[[analysis]][[model_name]]$results$real
@@ -107,15 +118,124 @@ data = data |>
       .default = estimate
     ),
     s_lcl = case_when(
-      Parameter == 'Phi' ~ lcl^12,
+      Parameter == 'S' ~ lcl^12,
       .default = lcl
     ),
     s_ucl = case_when(
-      Parameter == 'Phi' ~ ucl^12,
+      Parameter == 'S' ~ ucl^12,
       .default = ucl
     )
   )
 
 return(data)
 
+}
+
+build_base_plot = function(data, global_config, family_config, figure_config = list()){
+
+  #overide family_config with figure specific settings, if provided
+  config = modifyList(family_config, figure_config)
+
+  #TODO modify so that names are unique, right now some are being overridden
+  #set default save name if one is not provided
+  if (is.null(config$save_file_name)) {
+    facet_name = if (!is.null(config$facet_vars)) {
+      paste(config$facet_vars, collapse = "_")
+    } else {
+      'no_facet'
+    }
+    
+    config$save_file_name = sprintf(
+      '%s_%s_%s.png',
+      config$parameter,
+      config$y_label,
+      facet_name
+    )
+  }
+
+  #create save directory if needed
+  dir_create(config$save_folder)
+
+  #for readability
+  cm = global_config$column_mapping
+
+  #set dodge to make sure groups and error bars are on same alignment
+  dodge = position_dodge(width=0.9)
+
+  #filter to parameter of interest
+  p = data |> 
+  filter(.data[[cm$mark_parameter]]  == 
+    config$parameter) |> 
+  #base plot
+  ggplot(
+    aes(
+      x = factor(
+        .data[[config$x_factor]],
+        levels = config$x_order
+      ), #pull to global or family config?
+      y = .data[[config$y_factor]],
+      fill = factor(
+        .data[[config$grouping]],
+        levels = config$grouping_order
+      )
+    )
+  ) +
+  geom_col(position = dodge) +
+  labs(
+    x = config$x_factor_label,
+    y = config$y_label,
+    fill = config$grouping_label,
+    title = config$title,
+    subtitle = config$subtitle,
+    caption = config$caption
+  ) +
+  scale_fill_manual(
+    values = global_config$palettes[[config$grouping_palette]]
+  ) + 
+  global_config$theme
+  
+  # faceting logic
+  if (length(config$facet_vars) == 0) {
+    #do nothing to alter plot
+  } else if
+    (length(config$facet_vars) == 1) {
+    p = p +
+      facet_wrap(vars(.data[[config$facet_vars]]), scales = config$y_axis_scale)
+  } else if (length(config$facet_vars) == 2){
+    scales_setting = if (config$y_axis_scale == "free_full_y") "free_y" else config$y_axis_scale
+
+    independent_axis = if (config$y_axis_scale == "free_full_y") "y" else "none" 
+    p = p +
+      facet_grid2(
+        rows = vars(.data[[config$facet_vars[1]]]),
+        cols = vars(.data[[config$facet_vars[2]]]),
+        scales = scales_setting,
+        independent = independent_axis
+      )
+  } else {
+    stop(("facet_vars in family config file must have either 1, 2, or 0 variables"))
+  }
+
+  # add error bars if config flag is TRUE
+  if (config$variance_flag == TRUE) {
+    p = p +  
+      geom_errorbar(
+      aes(ymin = .data[[config$y_variance_lower]], ymax = .data[[config$y_variance_upper]]),
+      width = 0.2,
+      position = dodge
+      ) 
+    } else {
+  }
+
+
+  ggsave(
+  filename =  config$save_file_name,
+  plot = p,
+  path = config$save_folder,  
+  width = 8,
+  height = 5,
+  dpi = 300
+  )  
+
+  return(p)
 }
