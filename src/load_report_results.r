1175d455537e654f69fb49836bbd85bd12136c3c
Murray Hyde <jmhyde891@gmail.com>
2026-06-23 09:58:26 -0400

    doc: working abundance table

--- src/load_report_results.r
+++ src/load_report_results.r
@@ -229,8 +229,6 @@ make_survival_table = function(test_load) {
   fontsize(size = 8, part = "all") |> 
   padding(padding.top = 2, padding.bottom = 2, 
           padding.left = 3, padding.right = 3, part = "all") |> 
-  # width(j = 2:ncol(test_load), width = (6.5 - .8) / 10) |>  # distribute remaining width evenly across data cols
-  # width(j = 1, width = .8) 
   autofit() |> 
   fit_to_width(max_width = 6)
     # autofit() |> 
@@ -247,55 +245,21 @@ ft
 # function for making table object out of abundance tables for rendering 
 # Called in main document
 make_abundance_table = function(test_load) {
-  # occasions = c('Release', 'MR 1', 'MR 2', 'MR 3', 'MR 4')
-  # test_load = test_load |>
-  #   pivot_wider(
-  #     names_from = 'facility',
-  #     values_from = all_of(occasions),
-  #     names_glue = '{.value}_{facility}'
-  #   ) |> 
-  #   select(species, 
-  #        map(occasions, ~ paste0(.x, "_", c("FMCC", "Harrison Lake"))) |> unlist()
-  # )
-
   ft = flextable(test_load) |> 
-  #    set_header_labels(
-  #     `species` = "Species",
-  #     `Release_FMCC` = "FMCC",
-  #     `Release_Harrison Lake` = "Harrison Lake",
-  #     `MR 1_FMCC` = "FMCC",
-  #     `MR 1_Harrison Lake` = "Harrison Lake",
-  #     `MR 2_FMCC` = "FMCC",
-  #     `MR 2_Harrison Lake` = "Harrison Lake",
-  #     `MR 3_FMCC` = "FMCC",
-  #     `MR 3_Harrison Lake` = "Harrison Lake",
-  #     `MR 4_FMCC` = "FMCC",
-  #     `MR 4_Harrison Lake` = "Harrison Lake"
-  # ) |>
-  # # Add spanning top header row for occasions
-  # add_header_row(
-  #   values    = c("", occasions),
-  #   colwidths = c(1, rep(2, 5))
-  # ) |>
   # Style
   theme_vanilla() |>
+  merge_v(j = "Site") |> 
+  valign(j = "Site", valign = "top") |>
   align(align = "center", part = "header") |>
   align(align = 'center', part = 'body') |> 
   align(j = 1, align = "left", part = "all") |>
   fontsize(size = 8, part = "all") |> 
   padding(padding.top = 2, padding.bottom = 2, 
           padding.left = 3, padding.right = 3, part = "all") |> 
-  width(j = 2:ncol(test_load), width = (6.5 - .8) / 10) |>  # distribute remaining width evenly across data cols
-  width(j = 1, width = .8) 
-    # autofit() |> 
-  # fit_to_width(max_width = 6.5) |> 
-  # set_table_properties(
-  #   layout= 'fixed',
-  #   width = 1,
-  #   align = 'left'
-  # )
-
-ft
+  autofit() |> 
+  fit_to_width(max_width = 5.5)
+  
+  ft
 }
 
 
